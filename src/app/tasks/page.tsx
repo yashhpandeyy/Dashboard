6af7b1ec93da563df2cef01a5f1f94a5752f4717
Yash Pandey <ypandey7730@gmail.com>
2025-10-31 21:00:12 +0000

    make a seprate home page with only title Tasks and add it to the project

---
+++ src/app/tasks/page.tsx
@@ -0,0 +1,8 @@
+
+export default function TasksPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Tasks</h1>
+    </main>
+  );
+}
