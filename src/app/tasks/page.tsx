80d3985f61b0e12851bfdd31caab924bbd853fff
Yash Pandey <ypandey7730@gmail.com>
2025-10-31 21:01:54 +0000

    make task your heading not in center

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,7 +1,7 @@
 
 export default function TasksPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="p-12">
       <h1 className="text-4xl font-bold">Tasks</h1>
     </main>
   );
